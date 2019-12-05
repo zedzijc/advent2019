03cd341680b263b46da8d3afbdba3a5f18aaa4d0
Axner <anders.axner@live.se>
2019-12-05 14:56:13 +0100

    Add day 4 puzzle 2

--- solutions/day4/day4_solution.py
+++ solutions/day4/day4_solution.py
@@ -6,24 +6,47 @@ def get_int_by_index(number, from_index, to_index):
     return int(str(number)[from_index:to_index])
 
 
-def process_digit(index, max_index,
-                  previous_digit, min_digit,
-                  max_digit=10, double=False):
+def validate(password, additional_sequence_limitation):
+    repeated_letter_count = 1
+    previous_letter = None
+    sequence_sizes = []
+    for letter in password:
+        if letter == previous_letter:
+            repeated_letter_count += 1
+        else:
+            # If we are ending a sequence, store its size
+            if repeated_letter_count > 1:
+                sequence_sizes.append(repeated_letter_count)
+            previous_letter = letter
+            repeated_letter_count = 1
+    if repeated_letter_count > 1:
+        sequence_sizes.append(repeated_letter_count)
+    if not additional_sequence_limitation:
+        # Day 4 puzzle 1
+        return len(sequence_sizes) > 0
+    # Day 4 puzzle 2
+    # Verify that there is at least one sequence with length exactly 2
+    return len([sequence for sequence in sequence_sizes if sequence == 2]) > 0
+
+
+def process_digit(index, max_index, min_digit,
+                  max_digit=10, partial_password="", limitation=False):
     passwords = 0
     for digit in range(min_digit, max_digit):
+        password = partial_password + str(digit)
         # Are we at the end of the number?
         if index < max_index:
             # No, keep going.
-            passwords += process_digit(index + 1, max_index, digit, digit,
-                                       10, double or digit == previous_digit)
+            passwords += process_digit(index + 1, max_index, digit,
+                                       10, password, limitation)
         else:
             # Yes, have we satisfied the repeating numbers requirement?
-            if double or digit == previous_digit:
+            if validate(password, limitation):
                 passwords += 1
     return passwords
 
 
-def get_possible_passwords():
+def get_passwords(additional_sequence_limitation=False):
     min_password = 353096
     max_password = 843212
 
@@ -36,12 +59,21 @@ def get_possible_passwords():
     # Handle anything from 355555 up to 399999
     passwords += process_digit(second_index,
                                password_length,
-                               leading_min_digit,
-                               get_int_by_index(min_password, 1, 2))
+                               get_int_by_index(min_password, 1, 2),
+                               partial_password=str(leading_min_digit),
+                               limitation=additional_sequence_limitation)
     # Handle anything from 444444 to 799999
     passwords += process_digit(first_index,
                                password_length,
-                               None,
                                leading_min_digit + 1,
-                               leading_max_digit)
+                               max_digit=leading_max_digit,
+                               limitation=additional_sequence_limitation)
     print("Number of possible passwords are: {0}".format(passwords))
+
+
+def get_limited_passwords():
+    get_passwords(True)
+
+
+def get_possible_passwords():
+    get_passwords(False)
--- solutions/solver.py
+++ solutions/solver.py
@@ -1,14 +1,17 @@
 from solutions.day1.day1_solution import get_required_fuel, get_total_required_fuel
 from solutions.day2.day2_solution import get_intcode_value, get_moon_gravity_assist
 from solutions.day3.day3_solution import find_most_central_intersection, find_shortest_wiring_distance
-from solutions.day4.day4_solution import get_possible_passwords
+from solutions.day4.day4_solution import get_possible_passwords, get_limited_passwords
 
 
-solutions = {1: {1: get_required_fuel, 2: get_total_required_fuel},
-             2: {1: get_intcode_value, 2: get_moon_gravity_assist},
+solutions = {1: {1: get_required_fuel,
+                 2: get_total_required_fuel},
+             2: {1: get_intcode_value,
+                 2: get_moon_gravity_assist},
              3: {1: find_most_central_intersection,
                  2: find_shortest_wiring_distance},
-             4: {1: get_possible_passwords},
+             4: {1: get_possible_passwords,
+                 2: get_limited_passwords},
              5: {},
              6: {},
              7: {},
